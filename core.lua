b8493534ca03cd6f7ca58b40f9cf94bfa2d8bc4a
Torjus <nifty02@gmail.com>
2009-08-31 21:10:57 +0200

    rewrite

--- core.lua
+++ core.lua
@@ -3,92 +3,105 @@ local f = CreateFrame("Frame", "niftyMinimap", Minimap)
 f:RegisterEvent("PLAYER_LOGIN")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:RegisterEvent("CALENDAR_UPDATE_PENDING_INVITES")
-f:SetScript("OnEvent", function(self, event)
-    local time = GameTimeFrame
-    local pendingCalendarInvites = CalendarGetNumPendingInvites();
-    if event == "PLAYER_LOGIN" then 
-        f:UnregisterEvent("PLAYER_LOGIN")
-        
-        --Make it squara[] =]
-        Minimap:SetMaskTexture("Interface\\AddOns\\niftyminimap\\mask")
-        
-        --Damn tracker, so many frames and textures
-        MiniMapTracking:ClearAllPoints()
-        MiniMapTracking:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 5, -5)
-        MiniMapTrackingButtonBorder:Hide()
-        MiniMapTrackingBackground:Hide()
-        MiniMapTrackingButtonShine:Hide()
-        MiniMapTrackingIconOverlay:Hide()
-        
-        MinimapCluster:ClearAllPoints()
-        MinimapCluster:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", 0, -5)
-        
-        --The text
-        MinimapZoneText:ClearAllPoints()
-        MinimapZoneText:SetPoint("BOTTOM", Minimap, "BOTTOM", 0, 5)
-        MinimapZoneText:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-        MinimapZoneText:SetDrawLayer("OVERLAY")
-        
-        --Making it scroll!
-        self:EnableMouseWheel(true)
-        self:SetScript("OnMouseWheel", function(self, z)
-            local level = Minimap:GetZoom()
-            if level > 0 and z < 0 then
-                Minimap:SetZoom(level - 1)
-            elseif level < 5 and z > 0 then
-                Minimap:SetZoom(level + 1)
-            end
-        end)
 
-        time.string = time:GetFontString()
-        time.string:SetFont("Interface\\AddOns\\!LynSettings\\fonts\\font.ttf", 16, "OUTLINE")
-      --  time.string:SetTextColor(1, 1, 1)
-        time.string:SetShadowOffset(1, -1)
 
-        time:SetNormalTexture(nil)
-        time:SetPushedTexture(nil)
-        time:SetHighlightTexture(nil)
+f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 
-        time:ClearAllPoints()
-        time:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 5)
-        time:SetFrameStrata("DIALOG")
-        
-        --The black coolish border
-        self:ClearAllPoints()
-        self:SetPoint("CENTER", Minimap, "CENTER")
-        
-        self:SetFrameStrata("BACKGROUND")
-        self:SetFrameLevel(0)
-        
-        self:SetWidth(140)
-        self:SetHeight(140)
-        self:SetBackdrop({
-            bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
-            insets = { left = -2, top = -2, right = -2, bottom = -2},
-        })
-        self:SetBackdropColor(0,0,0,.5)
-        
-        --Hiding the minimap frames
-        local frames = {
-            MinimapZoomIn,
-            MinimapZoomOut,
-            MinimapToggleButton,
-            MinimapBorderTop,
-            MiniMapWorldMapButton,
-            MinimapBorder,
-        }
-        for _, frame in ipairs(frames) do 
-            frame:Hide()
-        end
-    end
-    if pendingCalendarInvites > 0 then 
-        time.string:SetTextColor(1, .4, 1)
-    else 
-        time.string:SetTextColor(1, 1, 1)
-    end
-    GameTimeCalendarInvitesTexture:Hide()
-    GameTimeCalendarInvitesGlow:Hide()
-    time.flashInvite = false;
+local string
+
+local function checkCalendarInvites ()
+	local pendingInvites = CalendarGetNumPendingInvites()
+	
+	if pendingInvites > 0 then
+		string:SetTextColor(1, .4, 1)
+	else
+		string:SetTextColor(1, 1, 1)
+	end
+end
+
+function f:PLAYER_LOGIN ()
+	self:UnregisterEvent("PLAYER_LOGIN")
+	
+	
+    self:ClearAllPoints()
+    self:SetPoint("CENTER", Minimap, "CENTER")
+    self:SetFrameStrata("BACKGROUND")
+    self:SetFrameLevel(0)
+    self:SetWidth(140)
+    self:SetHeight(140)
+    self:SetBackdrop({
+        bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+        insets = { left = -2, top = -2, right = -2, bottom = -2},
+    })
+    self:SetBackdropColor(0,0,0,.5)
+	
     
-    if event == "PLAYER_ENTERING_WORLD" then f:UnregisterEvent("PLAYER_ENTERING_WORLD") end
-end)
+	local Minimap = _G["Minimap"]	
+	Minimap:SetMaskTexture("Interface\\AddOns\\niftyminimap\\mask")
+	
+	
+	local tracker = _G["MiniMapTracking"]
+	tracker:ClearAllPoints()
+	tracker:SetPoint("TOPLEFT", Minimap, "TOPLEFT", 5, -5)
+	
+	
+	local cluster = _G["MinimapCluster"]
+	cluster:ClearAllPoints()
+	cluster:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", 0, -5)
+	
+	
+	local zone = _G["MinimapZoneText"]
+	zone:ClearAllPoints()
+	zone:SetPoint("BOTtom", Minimap, "BOTTOM", 0, 5)
+	zone:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+	zone:SetDrawLayer("OVERLAY")
+	
+	self:EnableMouseWheel(true)
+	self:SetScript("OnMouseWheel", function (self, z)
+		local level = Minimap:GetZoom()
+		if level > 0 and z < 0 then
+			Minimap:SetZoom(level -1)
+		elseif level < 5 and z > 0 then
+			Minimap:SetZoom(level + 1)
+		end
+	end)
+	
+	local frames = {
+		MinimapZoomIn,
+		MinimapZoomOut,
+        MinimapToggleButton,
+        MinimapBorderTop,
+        MiniMapWorldMapButton,
+        MinimapBorder,
+        MiniMapTrackingButtonBorder,
+        MiniMapTrackingBackground,
+        MiniMapTrackingButtonShine,
+        MiniMapTrackingIconOverlay,
+        GameTimeCalendarInvitesTexture,
+        GameTimeCalendarInvitesGlow        
+    }
+    for _, frame in pairs(frames) do frame:Hide() end
+end
+
+function f:PLAYER_ENTERING_WORLD ()
+
+	date = _G["GameTimeFrame"]
+	date:ClearAllPoints()
+	date:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 5)
+	date:SetFrameStrata("DIALOG")
+	
+	date:SetNormalTexture(nil)
+	date:SetPushedTexture(nil)
+	date:SetHighlightTexture(nil)
+	date.flashInvites = false
+	
+	string = date:GetFontString()
+	string:SetFont("Interface\\AddOns\\niftyminimap\\font.ttf", 16, "OUTLINE")
+	string:SetShadowOffset(1, -1)
+
+	checkCalendarInvites()	
+end
+
+function f:CALENDAR_UPDATE_PENDING_INVITES ()
+	checkCalendarInvites()
+end
